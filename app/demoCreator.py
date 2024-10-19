c4c7eb5e73b403d3ba3aee778b2c6b29954a7533
anacnoda77 <sm1108shin@naver.com>
2024-10-19 17:31:26 +0900

    Feat: 데모 데이터 생성 클래스 삽입

---
+++ app/demoCreator.py
@@ -0,0 +1,35 @@
+import redis
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from scheduler import RecommendationScheduler
+from eventQueue import EventQueue
+from recommender import VideoRecommender
+from datetime import datetime
+from fastapi.logger import logger
+import logging
+
+class DemoCreator:
+    def __init__(self, redis_client):
+        self.redis = redis_client
+        self.event_queue = EventQueue(self.redis)
+    
+    def make_demo_videos(self, recommender):
+        for i in range(10):
+            recommender.add_videoInfo(1, i, datetime.now())
+            logger.debug(f"Added video info: category 1, id {i}")
+    
+    def make_events(self, recommender):
+        for i in range(10):
+            for j in range(100):
+                if i % 2 == 0:
+                    recommender.get_new_event(1,1,i,True,False)
+                else:
+                    recommender.get_new_event(1,1,i,True,True)
+        
+        recommender.add_queue(1,1)
+    
+    
+    
+    def close(self):
+        self.redis.close()
